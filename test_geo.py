626890cbfe0038efe4d62d83e11003d9657f82b6
JamesNormandy <jamesnorman133@hotmail.com>
2022-02-08 13:31:15 +0000

    Tidied up formatting

--- test_geo.py
+++ test_geo.py
@@ -1,6 +1,6 @@
 
-from floodsystem.geo import stations_by_distance
-from floodsystem.geo import stations_within_radius
+from floodsystem.geo import *
+
 from floodsystem.stationdata import build_station_list
 
 
@@ -34,11 +34,11 @@ def test_rivers_with_station():
     stations = build_station_list()
     rivers = rivers_with_station(stations)
 
-    assert isinstance(rivers, set) #No duplicates possible
-    assert len(rivers) > 0 #Has been filled
+    assert isinstance(rivers, set)                                              # No duplicates possible
+    assert len(rivers) > 0                                                      # Has been filled
     
     for river in rivers:
-        assert isinstance(river, str) #Make sure names are in the set
+        assert isinstance(river, str)                                           # Make sure names are in the set
 
 def test_stations_by_river():
 
@@ -49,12 +49,12 @@ def test_stations_by_river():
     assert isinstance(river_stations, dict)
 
     for river, station_list in river_stations.items():
-        assert isinstance(river, str) #Are names
+        assert isinstance(river, str)                                           # Are names
         assert isinstance(station_list, list) #Is a list
 
         for station in station_list:
-            assert isinstance(station, MonitoringStation) #List of the correct object
-            assert station.river == river #Make sure river is correct
+            assert isinstance(station, MonitoringStation)                       # List of the correct object
+            assert station.river == river                                       # Make sure river is correct
 
 def test_rivers_by_station_number():
 
@@ -63,17 +63,17 @@ def test_rivers_by_station_number():
     stations = build_station_list()
     popular_rivers = rivers_by_station_number(stations, N)
 
-    assert isinstance(popular_rivers, list) #Is a list?
+    assert isinstance(popular_rivers, list)                                     # Is a list?
     
     no_of_stations = []
     for river in popular_rivers:
-        assert isinstance(river, tuple) #List contains tuples only?
-        assert isinstance(river[0], str) #First element of each tuple is a string?
-        assert isinstance(river[1], int) #Second element of each tuple is an integer?
+        assert isinstance(river, tuple)                                         # List contains tuples only?
+        assert isinstance(river[0], str)                                        # First element of each tuple is a string?
+        assert isinstance(river[1], int)                                        # Second element of each tuple is an integer?
         if river[1] not in no_of_stations:
             no_of_stations.append(river[1])
     
-    assert len(no_of_stations) == N #Make sure the greatest numbers of stations is exactly N.
+    assert len(no_of_stations) == N                                             # Make sure the greatest numbers of stations is exactly N.
 
 
 
