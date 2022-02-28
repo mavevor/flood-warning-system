89b95395528aa6bcf56e37d225b1e7bd59b9eb20
JamesNormandy <jamesnorman133@hotmail.com>
2022-02-28 17:23:30 +0000

    e & f complete

--- Task2E.py
+++ Task2E.py
@@ -1,12 +1,15 @@
+from logging import warning
 from floodsystem.plot import plot_water_levels
-from floodsystem.datafetcher import build_station_list, fetch_measure_levels
+from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import build_station_list, update_water_levels
 import datetime
 
 
 def run():
     
     all_stations = build_station_list()
+    update_water_levels(all_stations)
 
     N = 5
     dt = 10
@@ -15,6 +18,9 @@ def run():
 
     for station in highest_stations_rel:
         dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        if not dates and not levels:
+            print("Station's latest reading was more than {} days ago!".format(N))
+
         plot_water_levels(station, dates, levels)
 
 if __name__ == "__main__":
